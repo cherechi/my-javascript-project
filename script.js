3228544c0e50f853976388719603acb5a58bc8b0
CHERECHI NDUKWE <ndukwecherechi@yahoo.com>
2024-04-10 13:53:05 -0600

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Get the buttons by their IDs
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
+// Add click event listener to Button 1
+button1.addEventListener('click', function() {
+    alert('Hello');
+});
+
+// Add click event listener to Button 2
+button2.addEventListener('click', function() {
+    alert('Menh');
+});
