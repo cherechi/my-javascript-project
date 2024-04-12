45774e1e75df23c0263fabf95a8ab8b259b66ada
CHERECHI NDUKWE <ndukwecherechi@yahoo.com>
2024-04-12 08:21:18 -0600

    2nd Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Get the buttons by their IDs
-const button1 = document.getElementById('button1');
+/*const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 
 // Add click event listener to Button 1
@@ -10,4 +10,8 @@ button1.addEventListener('click', function() {
 // Add click event listener to Button 2
 button2.addEventListener('click', function() {
     alert('Dundas Catches this');
-});
+});*/
+function(){
+    alert('This works from Dundas');
+
+}
